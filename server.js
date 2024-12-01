73af445f40258a624505d791c2c97d4fc8f15c6b
sawa3030 <esawada2003@gmail.com>
2024-12-01 15:37:33 +0900

    added simple server

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/signup', (req, res) => {
+    const { user_id, password } = req.body;
+
+    if (!user_id || !password) {
+        return res.status(400).json({ 
+            message: 'Account creation failed',
+            cause: 'requried user_id and password'
+
+        });
+    }
+
+    res.status(201).json({        
+        message: "Account successfully created",
+        user: {
+            user_id: "TaroYamada",
+            nickname: "TaroYamada"
+        }
+        
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
