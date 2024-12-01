9b61ffc0dc52f1c23e8e6c08485e1114e4fc6598
sawa3030 <esawada2003@gmail.com>
2024-12-01 16:42:03 +0900

    added get users

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+const users = {
+    TaroYamada: {
+        password: "PaSSwd4TY",
+        nickname: "たろー",
+        comment: "僕は元気です",
+    },
+}
+
 app.post('/signup', (req, res) => {
     const { user_id, password } = req.body;
 
@@ -37,11 +45,54 @@ app.post('/signup', (req, res) => {
         });
     }
 
+    if (users[user_id]) {
+        return res.status(400).json({ 
+            message: "Account creation failed",
+            cause: "already same user_id used",
+        });
+    }
+
     res.status(200).json({        
         message: "Account successfully created",
         user: {
             user_id: user_id,
             nickname: user_id,
+        }
+    });
+
+    users[user_id] = {
+        password: password,
+        nickname: user_id,
+    };
+});
+
+app.get('/users/:user_id', (req, res) => {
+    const { user_id } = req.params;
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Basic ')) {
+        return res.status(401).json({ message: 'Authentication Failed' });
+    }
+
+    const encoded = authHeader.split(' ')[1];
+    const decoded = Buffer.from(encoded, 'base64').toString('utf-8'); 
+    const [authUserId, password] = decoded.split(':');
+
+    // if (authUserId !== user_id) {
+    //     return res.status(401).json({ message: 'Authentication Failed' });
+    // }
+
+    const user = users[authUserId];
+    if (!user) {
+        return res.status(404).json({ message: `No User found` });
+    }
+
+    res.status(200).json({
+        message: 'User details by user_id',
+        user: {
+            user_id: authUserId,
+            nickname: user.nickname,
+            comment: user.comment,
         }
     });
 });
